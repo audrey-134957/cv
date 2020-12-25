71017a731750a97f46e3a4a07f0e78e893a62ef9
audrey-134957 <milard.audrey@gmail.com>
2020-12-25 23:40:23 +0100

    inté en cours

--- js/initialize.js
+++ js/initialize.js
@@ -24,4 +24,47 @@ $(document).ready(function () {
         });
     });
 
+
+
+    $('.skills-table-headings__heading').on('click', function (e) {
+        console.log(this.id);
+        console.log(e.target);
+
+
+        $('.bar--french .bar--ahead').removeClass('bar--ahead--100');
+        $('.bar--french .bar--ahead').width();
+        $('.bar--french .bar--ahead').addClass('bar--ahead--100');
+
+        $('.bar--english .bar--ahead').removeClass('bar--ahead--40');
+        $('.bar--english .bar--ahead').width();
+        $('.bar--english .bar--ahead').addClass('bar--ahead--40');
+
+
+        $('.skills-table-headings__heading').each(function (i, obj) {
+            if (e.target.id == obj.id) {
+                // console.log('ok');
+                $(this).addClass('skills-table-headings__heading--active');
+                //si l'id du point ciblé est différent du point de l'objet
+            } else {
+                // console.log('no');
+                $(this).removeClass('skills-table-headings__heading--active');
+            }
+
+            if (e.target.id == obj.id && e.target.id == 'heading-2') {
+                $('.skills-table-headings__info-table-content').hide();
+                $('.skills-table-headings__language-table-content').show();
+
+            } else {
+
+                $('.skills-table-headings__info-table-content').show();
+                $('.skills-table-headings__language-table-content').hide();
+
+            }
+        });
+    });
+
+
+
+
+    // $('.')
 });
